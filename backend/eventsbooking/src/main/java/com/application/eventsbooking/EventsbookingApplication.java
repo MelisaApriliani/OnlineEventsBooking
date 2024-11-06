1e955035537c61d6f70c5fa81eb339c0236eac1f
MelisaApriliani <melisaapriliani30@gmail.com>
2024-11-06 20:47:23 +0700

    initialize backend project

---
+++ backend/eventsbooking/src/main/java/com/application/eventsbooking/EventsbookingApplication.java
@@ -0,0 +1,13 @@
+package com.application.eventsbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsbookingApplication.class, args);
+	}
+
+}
