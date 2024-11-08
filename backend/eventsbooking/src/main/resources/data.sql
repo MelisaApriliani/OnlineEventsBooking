28965e4a3c21e8c15138bd7cefef70ad6a1b6839
MelisaApriliani <melisaapriliani30@gmail.com>
2024-11-09 05:46:21 +0700

    insert predifined records in event status table

--- backend/eventsbooking/src/main/resources/data.sql
+++ backend/eventsbooking/src/main/resources/data.sql
@@ -1,4 +1,4 @@
--- INSERT TO USER TABLE
+-- INSERT TO account TABLE
 -- Check if the user table is empty, if so, insert predefined records
 INSERT INTO "account" (username, password, role)
 SELECT 'admin1@test.com', 'password123', 'ADMIN'
@@ -23,3 +23,17 @@ SELECT 'user2@test.com', 'password123', 'USER'
 INSERT INTO "account" (username, password, role)
 SELECT 'user3@test.com', 'password123', 'USER'
     WHERE NOT EXISTS (SELECT 1 FROM "account" WHERE username = 'user3@test.com');
+
+-- INSERT TO event_status TABLE
+-- Insert predefined statuses into event_status table only if they don't exist
+INSERT INTO event_status (id, status_name)
+SELECT 1, 'Approved'
+    WHERE NOT EXISTS (SELECT 1 FROM event_status WHERE id = 1);
+
+INSERT INTO event_status (id, status_name)
+SELECT 2, 'Pending'
+    WHERE NOT EXISTS (SELECT 1 FROM event_status WHERE id = 2);
+
+INSERT INTO event_status (id, status_name)
+SELECT 3, 'Rejected'
+    WHERE NOT EXISTS (SELECT 1 FROM event_status WHERE id = 3);
