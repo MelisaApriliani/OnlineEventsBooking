99cf0df5b9fa0a333d13fc90b6de2cc8be0d599b
MelisaApriliani <melisaapriliani30@gmail.com>
2024-11-08 18:44:29 +0700

    fix build errors

--- backend/eventsbooking/src/main/java/com/application/eventsbooking/controllers/UserController.java
+++ backend/eventsbooking/src/main/java/com/application/eventsbooking/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.application.eventsbooking.controllers;
 
+import com.application.eventsbooking.dto.BusinessEntityDetailsDTO;
 import com.application.eventsbooking.exception.UserNotFoundException;
 import com.application.eventsbooking.factory.BusinessEntityFactory;
 import com.application.eventsbooking.models.BusinessEntity;
@@ -43,7 +44,7 @@ public class UserController {
         BusinessEntityService businessEntityService = businessEntityFactory.getBusinessEntityService(user);
 
 
-        BusinessEntity businessDetails = businessEntityService.getBusinessEntityByUserId(id);
+        BusinessEntityDetailsDTO businessDetails = businessEntityService.getBusinessEntityByUserId(id);
 //        return ResponseEntity.ok("businessDetails.");
         // Retrieve and return user details (example code here)
         return "User details";
