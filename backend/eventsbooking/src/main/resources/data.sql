c40d491be7c89111d59ab57f8cfe853e66b91b9c
MelisaApriliani <melisaapriliani30@gmail.com>
2024-11-09 05:56:06 +0700

    insert predifined records to location table

--- backend/eventsbooking/src/main/resources/data.sql
+++ backend/eventsbooking/src/main/resources/data.sql
@@ -37,3 +37,33 @@ SELECT 2, 'Pending'
 INSERT INTO event_status (id, status_name)
 SELECT 3, 'Rejected'
     WHERE NOT EXISTS (SELECT 1 FROM event_status WHERE id = 3);
+
+-- INSERT TO location TABLE
+-- Insert predefined locations into location table only if they don't exist
+INSERT INTO location (id, name, postal_code, address)
+SELECT 1, 'CGC Hall', '10001', '5th Avenue, New York City'
+    WHERE NOT EXISTS (SELECT 1 FROM location WHERE id = 1 AND name = 'New York');
+
+INSERT INTO location (id, name, postal_code, address)
+SELECT 2, 'XYZ Convention Centre', '90001', 'Main Street, Los Angeles'
+    WHERE NOT EXISTS (SELECT 1 FROM location WHERE id = 2 AND name = 'Los Angeles');
+
+INSERT INTO location (id, name, postal_code, address)
+SELECT 3, 'One Avenue', '60601', 'Michigan Avenue, Chicago'
+    WHERE NOT EXISTS (SELECT 1 FROM location WHERE id = 3 AND name = 'Chicago');
+
+INSERT INTO location (id, name, postal_code, address)
+SELECT 4, 'City Hospital', '77001', 'Texas Street, Houston'
+    WHERE NOT EXISTS (SELECT 1 FROM location WHERE id = 4 AND name = 'Houston');
+
+INSERT INTO location (id, name, postal_code, address)
+SELECT 5, 'BYD Hall', '85001', 'Central Avenue, Phoenix'
+    WHERE NOT EXISTS (SELECT 1 FROM location WHERE id = 5 AND name = 'Phoenix');
+
+INSERT INTO location (id, name, postal_code, address)
+SELECT 6, 'UNITEN', '19101', 'Market Street, Philadelphia'
+    WHERE NOT EXISTS (SELECT 1 FROM location WHERE id = 6 AND name = 'Philadelphia');
+
+INSERT INTO location (id, name, postal_code, address)
+SELECT 7, 'San Antonio', '78201', 'Alamo Plaza, San Antonio'
+    WHERE NOT EXISTS (SELECT 1 FROM location WHERE id = 7 AND name = 'San Antonio');
