bad88d411ce6584c39504011c26383539f209c0a
MelisaApriliani <melisaapriliani30@gmail.com>
2024-11-08 22:24:04 +0700

    implement event controller

---
+++ backend/eventsbooking/src/main/java/com/application/eventsbooking/controllers/LocationController.java
@@ -0,0 +1,45 @@
+package com.application.eventsbooking.controllers;
+
+import com.application.eventsbooking.dto.ApiResponseDTO;
+import com.application.eventsbooking.dto.BusinessEntityDetailsDTO;
+import com.application.eventsbooking.dto.LocationDTO;
+import com.application.eventsbooking.exception.UserNotFoundException;
+import com.application.eventsbooking.factory.ApiResponseFactory;
+import com.application.eventsbooking.models.User;
+import com.application.eventsbooking.services.BusinessEntityService;
+import com.application.eventsbooking.services.LocationService;
+import com.application.eventsbooking.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/location")
+public class LocationController {
+
+    private final LocationService locationService;
+
+    @Autowired
+    public LocationController(LocationService locationService) {
+        this.locationService = locationService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<ApiResponseDTO<LocationDTO>> addLocation(@Valid @RequestBody LocationDTO locationDTO) {
+
+        LocationDTO responseDTO = locationService.addLocation(locationDTO);
+
+        return ApiResponseFactory.created(responseDTO, "Location added successfully.");
+    }
+
+    @GetMapping("")
+    public ResponseEntity<ApiResponseDTO<List<LocationDTO>>> getAllLocations() {
+
+        List<LocationDTO> responseDTO = locationService.getAllLocations();
+
+        return ApiResponseFactory.success(responseDTO, "Locations retrieved successfully.");
+    }
+}
